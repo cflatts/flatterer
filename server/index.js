85ffa629b272ecf0bf9d9e83fae241806f3239d0
Caitlin Flattery <caitlinflattery@gmail.com>
2022-04-07 19:10:41 -0500

    Define CRUD functions for users for web server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
 var express = require("express");
 var PORT = 3000;
 var app = express();
+app.get("/", function (req, resp) {
+    resp.send("DEFAULT ROUTE TBD");
+});
+/**
+ * USER FUNCTIONS
+ */
+app.post("/new/:username", function (req, resp) {
+    resp.send("CREATE A USER");
+});
+app.get("/users/:username", function (req, resp) {
+    resp.send("GET A USER");
+});
+app.post("/update/:userID", function (req, resp) {
+    resp.send("UPDATE A USER");
+});
+app["delete"]("/delete/:userID", function (req, resp) {
+    resp.send("DELETE A USER");
+});
 function init() {
     app.listen(PORT, function () {
         console.log("DEV server listening on port: ".concat(PORT));
--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,29 @@ const PORT: number = 3000;
 
 let app = express();
 
+app.get("/", (req, resp) => {
+    resp.send("DEFAULT ROUTE TBD");
+});
+
+/**
+ * USER FUNCTIONS
+ */
+app.post("/new/:username", (req, resp) => {
+    resp.send("CREATE A USER");
+});
+
+app.get("/users/:username", (req, resp) => {
+    resp.send("GET A USER");
+});
+
+app.post("/update/:userID", (req, resp) => {
+    resp.send("UPDATE A USER");
+});
+
+app.delete("/delete/:userID", (req, resp) => {
+    resp.send("DELETE A USER");
+});
+
 function init() {
     app.listen(PORT, () => {
         console.log(`DEV server listening on port: ${PORT}`);
